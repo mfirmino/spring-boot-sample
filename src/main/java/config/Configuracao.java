b814a4a83e90919722b2c897611724cae0b3d876
marcelofirmino <marcelo.firmino@reservafacil.tur.br>
2016-11-07 08:20:01 -0200

    Aula1

--- src/main/java/config/Configuracao.java
+++ src/main/java/config/Configuracao.java
@@ -1,8 +1,26 @@
-package main.java.config;
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by marcelofirmino on 31/10/16.
  */
+@SpringBootApplication
+@Controller
 public class Configuracao {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Configuracao.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String boasVindas(){
+        return "Olá, bem vindo ao sistema Lista VIPs";
+    }
 
 }

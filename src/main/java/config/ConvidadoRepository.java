e6ef60e2a70955c44a88ca0513c060b7a5c9ac38
marcelofirmino <marcelo.firmino@reservafacil.tur.br>
2017-02-08 17:01:36 -0200

    Aula 2 - parte 2

---
+++ src/main/java/config/ConvidadoRepository.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by mfirmino on 06/02/17.
+ */
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
+
+}
